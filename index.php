ffa52c0ebe0748713755d1b03f8283b954caa65c
Bryan Baker <bryanpbaker@gmail.com>
2015-12-01 16:52:52 -0600

    Added ability to create custom templates not associated with pages

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
-
+	<h1>hello world!</h1>
 
  	<?php if (locate_template('templates/pages/' . $post->post_name . '.php')): ?>
       <?php get_template_part('templates/pages/' . $post->post_name); ?>
